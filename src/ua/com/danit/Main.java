6390a605d68f2a1b2bc3de7b0445a7fc86bfac40
Nataliia Ivaniv <nat_ivaniv@ukr.net>
2020-08-15 15:05:34 +0300

    console (#2)

--- src/ua/com/danit/Main.java
+++ src/ua/com/danit/Main.java
@@ -1,7 +1,9 @@
 package ua.com.danit;
 
+import ua.com.danit.console.ConsoleMenu;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        ConsoleMenu.start();
     }
 }
